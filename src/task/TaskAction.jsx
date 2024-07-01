d32ff252b16e30096d5be2674ec7baa941dba54c
Jithan Roy <jithanroyjony@gmail.com>
2024-07-01 21:58:43 +0100

    modal creation

--- src/task/TaskAction.jsx
+++ src/task/TaskAction.jsx
@@ -1,9 +1,12 @@
-export const TaskAction = () => {
+export const TaskAction = ({ onSubmitAddTask }) => {
   return (
     <div className="mb-14 items-center justify-between sm:flex">
       <h2 className="text-2xl font-semibold max-sm:mb-4">Your Tasks</h2>
       <div className="flex items-center space-x-5">
-        <button className="rounded-md bg-blue-500 px-3.5 py-2.5 text-sm font-semibold">
+        <button
+          onClick={onSubmitAddTask}
+          className="rounded-md bg-blue-500 px-3.5 py-2.5 text-sm font-semibold"
+        >
           Add Task
         </button>
         <button className="rounded-md bg-red-500 px-3.5 py-2.5 text-sm font-semibold">
